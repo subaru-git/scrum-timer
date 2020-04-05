a80c766ff169fe5f5c8e956eb75924e38162e139
subaru-git <subaru12081985@yahoo.co.jp>
2020-04-05 20:32:31 +0900

    change name to GetSprintEndDate

--- src/SprintCalculator.test.ts
+++ src/SprintCalculator.test.ts
@@ -8,27 +8,27 @@ test('sprint calculator', () => {
   const start2 = moment('2020-04-03 16:06').toDate();
   const now2 = moment('2020-04-03 16:06').toDate();
   const end2 = moment('2020-04-03 18:00').toDate();
-  expect(GetEndDate(start, 1, now, end).getTime()).toBe(
+  expect(GetSprintEndDate(start, 1, now, end).getTime()).toBe(
     moment('2020-01-21 18:00')
       .toDate()
       .getTime(),
   );
-  expect(GetEndDate(start, 2, now, end).getTime()).toBe(
+  expect(GetSprintEndDate(start, 2, now, end).getTime()).toBe(
     moment('2020-01-28 18:00')
       .toDate()
       .getTime(),
   );
-  expect(GetEndDate(start, 3, now, end).getTime()).toBe(
+  expect(GetSprintEndDate(start, 3, now, end).getTime()).toBe(
     moment('2020-01-21 18:00')
       .toDate()
       .getTime(),
   );
-  expect(GetEndDate(start, 4, now, end).getTime()).toBe(
+  expect(GetSprintEndDate(start, 4, now, end).getTime()).toBe(
     moment('2020-01-28 18:00')
       .toDate()
       .getTime(),
   );
-  expect(GetEndDate(start2, 1, now2, end2).getTime()).toBe(
+  expect(GetSprintEndDate(start2, 1, now2, end2).getTime()).toBe(
     moment('2020-04-09 18:00')
       .toDate()
       .getTime(),
